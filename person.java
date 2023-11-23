ed419109334c10aac5749fe86b14ec40e0833f7c
amrish1255 <127399235+amrish1255@users.noreply.github.com>
2023-11-23 14:38:54 +0530

    student & student clint

---
+++ person.java
@@ -0,0 +1,19 @@
+package OPPS;
+
+public class person {
+	int age = 12;
+	String name = "Kaju";
+
+	public person() {
+//	block;
+
+	}
+
+// contructer 
+	
+	public person(String name, int age) {
+		this.name = "ggh";
+		this.age = 45;
+	}
+
+}
---
+++ person_clint.java
@@ -0,0 +1,19 @@
+package OPPS;
+//An object is defined is the data field that have unique attribute and behavour
+public class person_clint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hey");
+//		new person is an object 
+//		only person is class
+//		p is the variable name
+		person p = new person("raju", 12);
+		
+		System.out.println(p.name);
+		
+		System.out.println(p.age);
+	}
+
+}
